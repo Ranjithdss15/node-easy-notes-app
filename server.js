83db0c649dc2574f14edac1ff897ce39046ef0f2
Camillus Ranjith Doss <cpaul@altimetrik.com>
2019-10-25 20:58:54 +0530

    adding handlebars with server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var path = require('path');
-
+var exphbs = require('express-handlebars');
+var jsonParser = bodyParser.json();
 // create express app
 const app = express();
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
@@ -28,14 +34,23 @@ mongoose.connect(dbConfig.url, {
 });
 
 // define a simple route
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     //res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
     res.sendFile(path.join(__dirname,'pages','index.html'));
 });
-app.get('/new', (req, res) => {
-   
-    res.sendFile(path.join(__dirname,'pages','new.html'));
+*/
+
+app.get('/',(req,res,next) => {
+    res.render('home');
 });
+
+
+app.get('/new',(req,res,next) => {
+    res.render('home');
+});
+
+
+
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
